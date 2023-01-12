d201d7dd6cd688f2a895d59a9adf27dbbacd2a28
Samantha010222 <timbalsamanthaezra@gmail.com>
2023-01-12 22:26:28 +0800

    adding login page and adding dashboard assets

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,21 +1,33 @@
-@extends('layouts.app')
-
-@section('title', 'page title')
-
-@section('content')
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Landing Page - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href= "{{ asset('assets-homepage/assets/favicon.ico') }}"/>
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
+    </head>
+    <body>
         <nav class="navbar navbar-light bg-light static-top">
             <div class="container">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <a class="btn btn-primary" href="#signup">Sign Up</a>
+                <a class="navbar-brand" href="/">Welcome</a>
+                <a class="btn btn-primary" href="/login">Log in</a>
             </div>
         </nav>
         <!-- Masthead-->
-        <header class="masthead">
+       <header class="masthead1" style="background-image: url('assets-homepage/assets/img/bg-masthead1.jpg')">
             <div class="container position-relative">
-                <div class="row justify-content-center">
-                    <div class="col-xl-6">
-                        <div class="text-center text-white">
+                <div class="row justify-content-center ">
+                    <div class="col-xl-6" >
+                        <div class="text-center text-white" >
                             <!-- Page heading-->
                             <h1 class="mb-5">Generate more leads with a professional landing page!</h1>
                             <!-- Signup form-->
@@ -58,56 +70,28 @@
                 </div>
             </div>
         </header>
-        <!-- Icons Grid-->
-        <section class="features-icons bg-light text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-window m-auto text-primary"></i></div>
-                            <h3>Fully Responsive</h3>
-                            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
-                            <h3>Bootstrap 5 Ready</h3>
-                            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
-                            <h3>Easy to Use</h3>
-                            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
         <!-- Image Showcases-->
         <section class="showcase">
             <div class="container-fluid p-0">
                 <div class="row g-0">
-                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-1.jpg')"></div>
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets-homepage/assets/img/bg-showcase15.jpg')"></div>
                     <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-                        <h2>Fully Responsive Design</h2>
-                        <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+                        <h2>7/11 (seven-eleven)</h2>
+                        <p class="lead mb-0">is a popular convenience store that is open 24/7 all year. They sell a wide variety of snack products and other items that allow customers to quickly enter and exit the store; it is indeed convenient for customers to buy something in this store.</p>
                     </div>
                 </div>
                 <div class="row g-0">
-                    <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-2.jpg')"></div>
+                    <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets-homepage/assets/img/bg-showcase10.jpg')"></div>
                     <div class="col-lg-6 my-auto showcase-text">
-                        <h2>Updated For Bootstrap 5</h2>
-                        <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!</p>
+                        <h2>7/11 provides a competitive advantage in the market</h2>
+                        <p class="lead mb-0">through factors such as a brand's financial position, experienced workforce, product uniqueness, and intangible assets such as brand value.</p>
                     </div>
                 </div>
                 <div class="row g-0">
-                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-3.jpg')"></div>
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets-homepage/assets/img/bg-showcase11.jpg')"></div>
                     <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-                        <h2>Easy to Use & Customize</h2>
-                        <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+                        <h2>7-Eleven is the largest chain store in any category</h2>
+                        <p class="lead mb-0">opening approximately six stores per day somewhere in the world.</p>
                     </div>
                 </div>
             </div>
@@ -119,32 +103,32 @@
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-1.jpg" alt="..." />
-                            <h5>Margaret E.</h5>
-                            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+                            <img class="img-fluid rounded-circle mb-3" src="assets-homepage/assets/img/bg-person1.jpg" alt="..." />
+                            <h5>Ispyuti S.</h5>
+                            <p class="font-weight-light mb-0">"Lami ang sisig na tag 29 sa 7/11"</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-2.jpg" alt="..." />
-                            <h5>Fred S.</h5>
-                            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
+                            <img class="img-fluid rounded-circle mb-3" src="assets-homepage/assets/img/bg-person2.jpg" alt="..." />
+                            <h5>Dagsi J.</h5>
+                            <p class="font-weight-light mb-0">"daghay bb gurls na taga urios sa 7/11"</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-3.jpg" alt="..." />
-                            <h5>Sarah W.</h5>
-                            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+                            <img class="img-fluid rounded-circle mb-3" src="assets-homepage/assets/img/bg-person3.jpg" alt="..." />
+                            <h5>Andrei E.</h5>
+                            <p class="font-weight-light mb-0">"mahalin kah akong de gatas sa 7/11?"</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
         <!-- Call to Action-->
-        <section class="call-to-action text-white text-center" id="signup">
+        <section class="call-to-action text-white text-center" id="signup"  style="background-image: url('assets-homepage/assets/img/bg-masthead1.jpg')">
             <div class="container position-relative">
-                <div class="row justify-content-center">
+                <div class="row justify-content-center" >
                     <div class="col-xl-6">
                         <h2 class="mb-4">Ready to get started? Sign up now!</h2>
                         <!-- Signup form-->
@@ -205,7 +189,8 @@
                     <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
                         <ul class="list-inline mb-0">
                             <li class="list-inline-item me-4">
-                                <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                                <a href="https://www.facebook.com/711philippines/"><i class="bi-facebook fs-3"></i></a>
+                            
                             </li>
                             <li class="list-inline-item me-4">
                                 <a href="#!"><i class="bi-twitter fs-3"></i></a>
@@ -217,4 +202,10 @@
                     </div>
                 </div>
             </div>
-@endsection
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
